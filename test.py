9e6ad0d721ccbe3dccf90d5f98c6337624518e68
Nova <80784920+N3ST0R-byte@users.noreply.github.com>
2025-03-14 09:49:41 +0100

    changed the rainbow cycle

--- test.py
+++ test.py
@@ -48,9 +48,9 @@ def wheel(pos):
 
 
 def rainbow_cycle(wait):
-    for j in range(255):
-        for i in range(num_pixels):
-            pixel_index = (i * 256 // num_pixels) + j
+    for i in range(num_pixels):
+        if i == 1:
+            pixel_index = (i * 256 // num_pixels)
             pixels[i] = wheel(pixel_index & 255)
         pixels.show()
         time.sleep(wait)
